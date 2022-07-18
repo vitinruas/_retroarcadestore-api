284b7ccbe95d111564d240aca9b9904b71933426
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-18 20:54:07 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup-controller'
+
+describe('SignUpController', () => {
+  test('should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const request = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const response = sut.perform(request)
+    expect(response.code).toBe(400)
+    expect(response.body).toBe('Missing param: name')
+  })
+})
