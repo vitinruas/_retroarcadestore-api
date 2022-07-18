74a787700d9b5a1171f1be5bf373575ab72ed084
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-18 20:58:15 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -28,4 +28,18 @@ describe('SignUpController', () => {
     expect(response.code).toBe(400)
     expect(response.body).toBe('Missing param: email')
   })
+
+  test('should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const request = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const response = sut.perform(request)
+    expect(response.code).toBe(400)
+    expect(response.body).toBe('Missing param: password')
+  })
 })
