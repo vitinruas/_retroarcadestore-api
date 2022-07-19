7cc474ff9bb2fbaf48663810e91b09109ea464ba
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-19 08:42:17 -0300

    feat: ensure AddAccountUseCase returns throw if GetAccountByEmailRepository throws

--- src/usecases/account/add-account-usecase.spec.ts
+++ src/usecases/account/add-account-usecase.spec.ts
@@ -1,4 +1,6 @@
+import { IAccountEntitie } from 'src/domain/entities/account'
 import { IAddAccountModel } from 'src/domain/usecases/account/add-account-usecase'
+import { IGetAccountByEmailRepository } from '../protocols/account/get-account-by-email-repository'
 import { IHasher } from '../protocols/account/hasher-protocol'
 import { AddAccountUseCase } from './add-account-usecase'
 
@@ -17,17 +19,40 @@ const makePasswordHasherStub = () => {
   return new PasswordHasherStub()
 }
 
+const makeGetAccountByEmailRepositoryStub = () => {
+  class GetAccountByEmailRepositoryStub
+    implements IGetAccountByEmailRepository
+  {
+    async get(email: string): Promise<IAccountEntitie | null> {
+      return Promise.resolve({
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        isAdmin: false
+      })
+    }
+  }
+  return new GetAccountByEmailRepositoryStub()
+}
+
 interface ISut {
   sut: AddAccountUseCase
   passwordHasherStub: IHasher
+  getAccountByEmailRepositoryStub: IGetAccountByEmailRepository
 }
 
 const makeSut = (): ISut => {
+  const getAccountByEmailRepositoryStub = makeGetAccountByEmailRepositoryStub()
   const passwordHasherStub = makePasswordHasherStub()
-  const sut = new AddAccountUseCase(passwordHasherStub)
+  const sut = new AddAccountUseCase(
+    getAccountByEmailRepositoryStub,
+    passwordHasherStub
+  )
   return {
     sut,
-    passwordHasherStub
+    passwordHasherStub,
+    getAccountByEmailRepositoryStub
   }
 }
 
@@ -35,6 +60,7 @@ describe('AddAccountUseCase', () => {
   test('should calls PasswordHasher with a password', async () => {
     const { sut, passwordHasherStub } = makeSut()
     const hashSpy = jest.spyOn(passwordHasherStub, 'hash')
+
     await sut.add(makeValidNewAccountData())
 
     expect(hashSpy).toHaveBeenCalledWith('any_password')
@@ -45,6 +71,27 @@ describe('AddAccountUseCase', () => {
     jest
       .spyOn(passwordHasherStub, 'hash')
       .mockImplementationOnce(async () => Promise.reject(new Error()))
+
+    const promise = sut.add(makeValidNewAccountData())
+
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('should call GetAccountByEmailRepository with correct values', async () => {
+    const { sut, getAccountByEmailRepositoryStub } = makeSut()
+    const getSpy = jest.spyOn(getAccountByEmailRepositoryStub, 'get')
+
+    await sut.add(makeValidNewAccountData())
+
+    expect(getSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  test('should return throw if GetAccountByEmailRepository throws', async () => {
+    const { sut, getAccountByEmailRepositoryStub } = makeSut()
+    jest
+      .spyOn(getAccountByEmailRepositoryStub, 'get')
+      .mockImplementationOnce(async () => Promise.reject(new Error()))
+
     const promise = sut.add(makeValidNewAccountData())
 
     await expect(promise).rejects.toThrow()
