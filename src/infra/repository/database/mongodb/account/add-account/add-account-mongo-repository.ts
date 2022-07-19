894093d0b57dbc83fc83474ef110b47271848b48
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-19 20:25:42 -0300

    feat: ensure AddAccountMongoRepository add an account and return it

---
+++ src/infra/repository/database/mongodb/account/add-account/add-account-mongo-repository.ts
@@ -0,0 +1,23 @@
+import {
+  IAccountEntitie,
+  IAddAccountModel,
+  IAddAccountRepository
+} from 'src/usecases/account/add-account-protocols'
+import accountHelper from '../../helpers/account-helper'
+import mongoHelper from '../../helpers/mongo-helper'
+
+export class AddAccountMongoRepository implements IAddAccountRepository {
+  async add(newAccountData: IAddAccountModel): Promise<IAccountEntitie> {
+    const collectionRef = mongoHelper.getCollection('accounts')
+    const createdAccountID = (
+      await collectionRef.insertOne({
+        ...newAccountData,
+        createdAt: accountHelper.getCurrentTime()
+      })
+    ).insertedId
+    const createdAccount = await collectionRef.findOne({
+      _id: createdAccountID
+    })
+    return mongoHelper.replaceMongoID(createdAccount)
+  }
+}
