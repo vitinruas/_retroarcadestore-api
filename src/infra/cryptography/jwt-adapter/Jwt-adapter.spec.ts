eac3b4f8f25d76f23984905d25b446c1e399bac0
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-19 12:11:48 -0300

    test: ensure JwtAdapter returns a token

---
+++ src/infra/cryptography/jwt-adapter/Jwt-adapter.spec.ts
@@ -0,0 +1,16 @@
+import 'jsonwebtoken'
+import { JwtAdapter } from './jwt-adapter'
+
+jest.mock('jsonwebtoken', () => ({
+  sign(): string {
+    return 'any_token'
+  }
+}))
+
+describe('JwtAdapter', () => {
+  test('should return a token', async () => {
+    const sut = new JwtAdapter('secretKey')
+    const token = await sut.encrypt('any_id')
+    expect(token).toBe('any_token')
+  })
+})
