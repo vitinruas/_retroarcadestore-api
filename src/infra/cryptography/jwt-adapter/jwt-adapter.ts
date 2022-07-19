445f9bc9d33b037f85abe13d6881b510a95d64c6
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-19 12:11:32 -0300

    feat: ensure JwtAdapter returns a token

---
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,11 @@
+import { IEncrypter } from 'src/usecases/account/add-account-protocols'
+import jwt from 'jsonwebtoken'
+
+export class JwtAdapter implements IEncrypter {
+  constructor(private readonly secret: string) {}
+
+  async encrypt(id: string): Promise<string> {
+    const token = jwt.sign(id, this.secret)
+    return token
+  }
+}
