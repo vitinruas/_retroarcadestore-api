2b1b7f673989b5d644890cebe3160188e8ab7e07
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-18 21:59:27 -0300

    feat: ensure SignUpController calls EmailValidator with an email

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -6,18 +6,31 @@ import {
   IHttpRequest,
   IHttpResponse
 } from 'src/presentation/protocols/http-protocol'
+import { IEmailValidator } from 'src/presentation/protocols/email-validator-protocol'
 
 export class SignUpController implements IController {
+  constructor(private readonly emailValidatorStub: IEmailValidator) {}
+
   perform(httpRequest: IHttpRequest): IHttpResponse {
+    // check if all required fields has been provided
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const requiredField of requiredFields) {
       if (!httpRequest.body[requiredField]) {
         return badRequest(new MissingFieldError(requiredField))
       }
     }
+
+    // check if passwords match
     if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
       return badRequest(new InvalidFieldError('passwordConfirmation'))
     }
+
+    const isValid = this.emailValidatorStub.validate(httpRequest.body.email)
+
+    if (!isValid) {
+      return badRequest(new InvalidFieldError('email'))
+    }
+
     return ok()
   }
 }
