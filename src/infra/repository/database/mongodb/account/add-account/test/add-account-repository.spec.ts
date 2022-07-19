c064b528d7e61300a562495f68d0d429a0e0f064
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-19 20:26:12 -0300

    test: ensure AddAccountMongoRepository add an account and return it

---
+++ src/infra/repository/database/mongodb/account/add-account/test/add-account-repository.spec.ts
@@ -0,0 +1,45 @@
+import { AddAccountMongoRepository } from '../add-account-mongo-repository'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import { Collection } from 'mongoose'
+import mongoHelper from '../../../helpers/mongo-helper'
+import { IAddAccountModel } from 'src/usecases/account/add-account-protocols'
+
+let mongod: MongoMemoryServer
+let collectionRef: Collection
+
+beforeAll(async () => {
+  mongod = await MongoMemoryServer.create()
+  const uri = mongod.getUri()
+  await mongoHelper.connect(uri)
+})
+
+afterAll(async () => {
+  await mongoHelper.disconnect()
+  await mongod.stop()
+})
+
+beforeEach(async () => {
+  collectionRef = mongoHelper.getCollection('accounts')
+  await collectionRef.deleteMany({})
+})
+
+const makeSut = (): AddAccountMongoRepository => {
+  return new AddAccountMongoRepository()
+}
+
+const makeFakeValidAccount = (): IAddAccountModel => ({
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'hashed_password'
+})
+
+describe('AddAccountMongoRepository', () => {
+  test('should add an account and return it', async () => {
+    const sut = makeSut()
+    const createdAccount = await sut.add(makeFakeValidAccount())
+    expect(createdAccount.id).toBeTruthy()
+    expect(createdAccount.name).toBe('any_name')
+    expect(createdAccount.email).toBe('any_email@mail.com')
+    expect(createdAccount.password).toBe('hashed_password')
+  })
+})
