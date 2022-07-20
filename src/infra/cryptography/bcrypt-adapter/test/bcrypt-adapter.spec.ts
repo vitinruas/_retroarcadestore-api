69ab6fb3b4732c0eb6795c3cba3547f2550de6fe
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-20 11:42:11 -0300

    test: ensure BcryptAdapter returns true Bcrypt.compare succeeds

--- src/infra/cryptography/bcrypt-adapter/test/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter/test/bcrypt-adapter.spec.ts
@@ -1,16 +1,28 @@
 import { BcryptAdapter } from '../bcrypt-adapter'
-import 'bcrypt'
 
 jest.mock('bcrypt', () => ({
   hash(): string {
-    return 'hashed_password'
+    return 'hashed_value'
+  },
+
+  compare(): Promise<boolean> {
+    return Promise.resolve(true)
   }
 }))
 
 describe('BcryptAdapter', () => {
-  test('should return a hashed password', async () => {
-    const sut = new BcryptAdapter(12)
-    const hashedPassword = await sut.hash('any_password')
-    expect(hashedPassword).toBe('hashed_password')
+  describe('hash', () => {
+    test('should return a hashed password', async () => {
+      const sut = new BcryptAdapter(12)
+      const hashedPassword = await sut.hash('any_value')
+      expect(hashedPassword).toBe('hashed_value')
+    })
+  })
+  describe('compare', () => {
+    test('should return true if Bcrypt.compare succeeds', async () => {
+      const sut = new BcryptAdapter(12)
+      const hashedPassword = await sut.compare('any_value', 'hashed_value')
+      expect(hashedPassword).toBe(true)
+    })
   })
 })
