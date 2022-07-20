6962bc4d2ea4444b1a6fa76d02b80a27c5c01d5a
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-20 11:39:25 -0300

    feat: ensure BcryptAdapter returns true Bcrypt.compare succeeds

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,12 +1,18 @@
 import { IHasher } from '../../../usecases/protocols/cryptography/hasher-protocol'
 
 import bcrypt from 'bcrypt'
+import { IHashComparer } from '../../../usecases/protocols/cryptography/hash-comparer-protocol'
 
-export class BcryptAdapter implements IHasher {
+export class BcryptAdapter implements IHasher, IHashComparer {
   constructor(private readonly salt: number) {}
 
   async hash(password: string): Promise<string> {
     const hashedPassword = await bcrypt.hash(password, this.salt)
     return hashedPassword
+  }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
   }
 }
