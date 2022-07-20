bf5a64034ccda984d773d1179fa7c3bdb8cc00c4
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-20 08:03:38 -0300

    test: ensure LoginController calls EmailValidator with an email

--- src/presentation/controllers/authentication/login/test/login-controller.spec.ts
+++ src/presentation/controllers/authentication/login/test/login-controller.spec.ts
@@ -1,9 +1,10 @@
-import { MissingFieldError } from '../../../../errors'
+import { InvalidFieldError, MissingFieldError } from '../../../../errors'
 import { badRequest } from '../../../../helpers/http-response-helper'
 import {
   IHttpRequest,
   IHttpResponse
 } from '../../../../protocols/http-protocol'
+import { IEmailValidatorAdapter } from '../../signup/signup-controller-protocols'
 import { LoginController } from '../login-controller'
 
 const makeValidRequest = (): IHttpRequest => ({
@@ -13,9 +14,33 @@ const makeValidRequest = (): IHttpRequest => ({
   }
 })
 
+const makeEmailValidatorAdapterStub = (): IEmailValidatorAdapter => {
+  class EmailValidatorAdapterStub implements IEmailValidatorAdapter {
+    validate(email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorAdapterStub()
+}
+
+interface ISut {
+  sut: LoginController
+  emailValidatorAdapterStub: IEmailValidatorAdapter
+}
+
+const makeSut = (): ISut => {
+  const emailValidatorAdapterStub: IEmailValidatorAdapter =
+    makeEmailValidatorAdapterStub()
+  const sut: LoginController = new LoginController(emailValidatorAdapterStub)
+  return {
+    sut,
+    emailValidatorAdapterStub
+  }
+}
+
 describe('LoginController', () => {
   test('should return 400 if no email is provided', async () => {
-    const sut = new LoginController()
+    const { sut } = makeSut()
     const httpRequest: IHttpRequest = {
       body: {
         password: 'any_password'
@@ -27,7 +52,7 @@ describe('LoginController', () => {
   })
 
   test('should return 400 if no password is provided', async () => {
-    const sut = new LoginController()
+    const { sut } = makeSut()
     const httpRequest: IHttpRequest = {
       body: {
         email: 'any_email@mail.com'
@@ -36,5 +61,21 @@ describe('LoginController', () => {
     const httpResponse: IHttpResponse = await sut.perform(httpRequest)
 
     expect(httpResponse).toEqual(badRequest(new MissingFieldError('password')))
+  })
+
+  test('should call EmailValidator with an email', async () => {
+    const { sut, emailValidatorAdapterStub } = makeSut()
+    const httpRequest: IHttpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const validateSpy = jest
+      .spyOn(emailValidatorAdapterStub, 'validate')
+      .mockReturnValueOnce(false)
+    await sut.perform(httpRequest)
+
+    expect(validateSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
