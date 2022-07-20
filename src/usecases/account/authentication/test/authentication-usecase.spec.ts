d39800a9c32777196b93413f05729d3dcf5f40ab
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-20 10:04:12 -0300

    test: ensure AuthenticationUseCase calls GetAccountByEmailRepository with an email

---
+++ src/usecases/account/authentication/test/authentication-usecase.spec.ts
@@ -0,0 +1,50 @@
+import { IAuthenticationModel } from '../../../../domain/usecases/account/authentication-usecase'
+import {
+  IAccountEntitie,
+  IGetAccountByEmailRepository
+} from '../../add-account/add-account-usecase-protocols'
+import { AuthenticationUseCase } from '../authentication-usecase'
+
+const makeFakeValidAuthenticationData = (): IAuthenticationModel => ({
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
+const makeGetAccountByEmailRepositoryStub = () => {
+  class GetAccountByEmailRepositoryStub
+    implements IGetAccountByEmailRepository
+  {
+    async get(email: string): Promise<IAccountEntitie | null> {
+      return Promise.resolve(null)
+    }
+  }
+  return new GetAccountByEmailRepositoryStub()
+}
+
+interface ISut {
+  sut: AuthenticationUseCase
+  getAccountByEmailRepositoryStub: IGetAccountByEmailRepository
+}
+
+const makeSut = (): ISut => {
+  const getAccountByEmailRepositoryStub: IGetAccountByEmailRepository =
+    makeGetAccountByEmailRepositoryStub()
+  const sut: AuthenticationUseCase = new AuthenticationUseCase(
+    getAccountByEmailRepositoryStub
+  )
+  return {
+    sut,
+    getAccountByEmailRepositoryStub
+  }
+}
+
+describe('AuthenticationUseCase', () => {
+  test('should calls GetAccountByEmailRepository with an email', async () => {
+    const { sut, getAccountByEmailRepositoryStub } = makeSut()
+    const getSpy = jest.spyOn(getAccountByEmailRepositoryStub, 'get')
+
+    await sut.authenticate(makeFakeValidAuthenticationData())
+
+    expect(getSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+})
