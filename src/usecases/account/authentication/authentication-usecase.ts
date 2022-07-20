ecb9704dfc262ae280076c0d6c83f269998d5a38
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-20 10:28:54 -0300

    feat: ensure AuthenticationUseCase calls PasswordHashComparer with correct values

--- src/usecases/account/authentication/authentication-usecase.ts
+++ src/usecases/account/authentication/authentication-usecase.ts
@@ -2,20 +2,29 @@ import {
   IAuthenticationModel,
   IAuthenticationUseCase
 } from '../../../domain/usecases/account/authentication-usecase'
-import { IGetAccountByEmailRepository } from '../add-account/add-account-usecase-protocols'
+import { IHashComparer } from '../../protocols/cryptography/hash-comparer-protocol'
+import {
+  IAccountEntitie,
+  IGetAccountByEmailRepository
+} from '../add-account/add-account-usecase-protocols'
 
 export class AuthenticationUseCase implements IAuthenticationUseCase {
   constructor(
-    private readonly getAccountByEmailRepository: IGetAccountByEmailRepository
+    private readonly getAccountByEmailRepository: IGetAccountByEmailRepository,
+    private readonly passwordHashComparer: IHashComparer
   ) {}
 
   async authenticate(
     authenticationData: IAuthenticationModel
   ): Promise<string | null> {
-    const account = await this.getAccountByEmailRepository.get(
+    const account: IAccountEntitie = await this.getAccountByEmailRepository.get(
       authenticationData.email
     )
     if (account) {
+      await this.passwordHashComparer.compare(
+        authenticationData.password,
+        account.password
+      )
     }
     return null
   }
