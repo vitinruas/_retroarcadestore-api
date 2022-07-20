5d2b782d01cc86656029bf5f57b41ad023bb9fd6
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-20 11:43:53 -0300

    test: ensure BcryptAdapter calls Bcrypt.compare with correct values

--- src/infra/cryptography/bcrypt-adapter/test/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter/test/bcrypt-adapter.spec.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import { BcryptAdapter } from '../bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
@@ -14,14 +15,27 @@ describe('BcryptAdapter', () => {
   describe('hash', () => {
     test('should return a hashed password', async () => {
       const sut = new BcryptAdapter(12)
+
       const hashedPassword = await sut.hash('any_value')
+
       expect(hashedPassword).toBe('hashed_value')
     })
   })
   describe('compare', () => {
+    test('should call Bcrypt.compare with correct values', async () => {
+      const sut = new BcryptAdapter(12)
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
+
+      await sut.compare('any_value', 'hashed_value')
+
+      expect(compareSpy).toHaveBeenCalledWith('any_value', 'hashed_value')
+    })
+
     test('should return true if Bcrypt.compare succeeds', async () => {
       const sut = new BcryptAdapter(12)
+
       const hashedPassword = await sut.compare('any_value', 'hashed_value')
+
       expect(hashedPassword).toBe(true)
     })
   })
