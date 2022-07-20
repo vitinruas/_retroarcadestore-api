e74463469a6a15ddff0f203e2417c356723585a6
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-20 07:42:13 -0300

    feat: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/authentication/login/login-controller.ts
@@ -0,0 +1,19 @@
+import { MissingFieldError } from '../../../errors'
+import { badRequest } from '../../../helpers/http-response-helper'
+import {
+  IController,
+  IHttpRequest,
+  IHttpResponse
+} from '../signup/signup-controller-protocols'
+
+export class LoginController implements IController {
+  async perform(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingFieldError('email'))
+    }
+    return Promise.resolve({
+      statusCode: 200,
+      body: null
+    })
+  }
+}
