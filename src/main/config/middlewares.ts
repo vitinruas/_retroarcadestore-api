c6dbe011c1c549c9d44687fc6b445b3ff4934769
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-19 21:56:25 -0300

    feat: should return json content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,8 @@
 import { Express } from 'express'
 import { jsonMiddleware } from '../middlewares/body-parser/body-parser-middleware'
+import { contentTypeMiddleware } from '../middlewares/content-type/content-type-middleware'
+
 export default (app: Express) => {
   app.use(jsonMiddleware())
+  app.use(contentTypeMiddleware)
 }
