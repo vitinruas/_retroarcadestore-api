db72dcb237146870ffbeb42aa95027799721225f
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-20 10:56:36 -0300

    feat: ensure AuthenticationUseCase calls TokenGeneratorAdapter with correct values

--- src/usecases/account/authentication/authentication-usecase.ts
+++ src/usecases/account/authentication/authentication-usecase.ts
@@ -5,13 +5,15 @@ import {
 import { IHashComparer } from '../../protocols/cryptography/hash-comparer-protocol'
 import {
   IAccountEntitie,
+  IEncrypter,
   IGetAccountByEmailRepository
 } from '../add-account/add-account-usecase-protocols'
 
 export class AuthenticationUseCase implements IAuthenticationUseCase {
   constructor(
     private readonly getAccountByEmailRepository: IGetAccountByEmailRepository,
-    private readonly passwordHashComparer: IHashComparer
+    private readonly passwordHashComparerAdapter: IHashComparer,
+    private readonly tokenGeneratorAdapter: IEncrypter
   ) {}
 
   async authenticate(
@@ -21,11 +23,12 @@ export class AuthenticationUseCase implements IAuthenticationUseCase {
       authenticationData.email
     )
     if (account) {
-      const isValid = await this.passwordHashComparer.compare(
+      const isValid = await this.passwordHashComparerAdapter.compare(
         authenticationData.password,
         account.password
       )
       if (isValid) {
+        await this.tokenGeneratorAdapter.encrypt(account.id)
       }
     }
     return null
