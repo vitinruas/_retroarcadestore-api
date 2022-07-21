13e8cc3ae0e3981594a25da7c5ffefbfed221123
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-21 19:43:33 -0300

    style: add comments to CheckAccessTokenUseCase

--- src/usecases/account/check-access-token/check-access-token-usecase.ts
+++ src/usecases/account/check-access-token/check-access-token-usecase.ts
@@ -11,14 +11,17 @@ export class CheckAccessTokenUseCase implements ICheckAccessTokenUseCase {
   ) {}
 
   async check(accessToken: string): Promise<IAccountEntitie | null> {
+    // check if provided access token is valid and return it
     const isValid: string | null = await this.tokenDecrypterAdapter.decrypt(
       accessToken
     )
     if (isValid) {
+      // check if there are account with this access token
       const account = await this.getAccountByAccessTokenRepository.get(
         accessToken,
         this.isAdmin
       )
+      // return an account if all validations above succeeds
       if (account) {
         return account
       }
