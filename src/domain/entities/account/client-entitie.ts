4d88a9530c790d5e5fc7bb22e143ffcffac935a5
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-24 15:09:47 -0300

    feat: add client entitie

---
+++ src/domain/entities/account/client-entitie.ts
@@ -0,0 +1,15 @@
+import { IAccountEntitie } from './account-entitie'
+
+export interface IAddress {
+  street: string
+  postalCode: number
+  district: string
+  city: string
+  country: string
+  complement: string
+}
+
+export interface IClientEntitie extends IAccountEntitie {
+  photo: string
+  address: IAddress
+}
