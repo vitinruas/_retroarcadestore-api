177b2ddfa296f675fc65dc26c48ecca99b349342
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-24 16:17:23 -0300

    feat: ensure GetAccountController returns 500 if GetAccountUseCase throws

--- src/presentation/controllers/account/get-account/get-account-controller.ts
+++ src/presentation/controllers/account/get-account/get-account-controller.ts
@@ -1,5 +1,5 @@
 import { IGetAccountUseCase } from '../../../../domain/usecases/account/get-account-usecase'
-import { ok } from '../../../helpers/http-response-helper'
+import { ok, serverError } from '../../../helpers/http-response-helper'
 import {
   IController,
   IHttpRequest,
@@ -10,7 +10,11 @@ export class GetAccountController implements IController {
   constructor(private readonly getAccountUseCase: IGetAccountUseCase) {}
 
   async perform(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    await this.getAccountUseCase.get(httpRequest.body?.id)
-    return ok()
+    try {
+      await this.getAccountUseCase.get(httpRequest.body?.id)
+      return ok()
+    } catch (error: any) {
+      return serverError(error)
+    }
   }
 }
