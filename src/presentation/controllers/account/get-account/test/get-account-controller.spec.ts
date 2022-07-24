749a3407ba0a9664aecf63b95780fea27816933a
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-24 16:09:06 -0300

    refactor: remove code duplication from GetAccountController test

--- src/presentation/controllers/account/get-account/test/get-account-controller.spec.ts
+++ src/presentation/controllers/account/get-account/test/get-account-controller.spec.ts
@@ -3,29 +3,48 @@ import { IAccountEntitie } from '../../../../middlewares/auth-middleware-protoco
 import { IHttpRequest } from '../../authentication/login/login-controller-protocols'
 import { GetAccountController } from '../get-account-controller'
 
+const makeFakeValidRequest = (): IHttpRequest => ({
+  body: {
+    id: 'any_id'
+  }
+})
+
+const makeGetAccountUseCaseStub = (): IGetAccountUseCase => {
+  class GetAccountUseCaseStub implements IGetAccountUseCase {
+    get(id: string): Promise<IAccountEntitie> {
+      return Promise.resolve({
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'hashed_password',
+        accessToken: 'any_token'
+      })
+    }
+  }
+  return new GetAccountUseCaseStub()
+}
+
+interface ISut {
+  sut: GetAccountController
+  getAccountUseCaseStub: IGetAccountUseCase
+}
+
+const makeSut = (): ISut => {
+  const getAccountUseCaseStub: IGetAccountUseCase = makeGetAccountUseCaseStub()
+  const sut = new GetAccountController(getAccountUseCaseStub)
+  return {
+    sut,
+    getAccountUseCaseStub
+  }
+}
+
 describe('GetAccountUseCase', () => {
   test('should call GetAccountUseCase with an user id', async () => {
-    class GetAccountUseCaseStub implements IGetAccountUseCase {
-      get(id: string): Promise<IAccountEntitie> {
-        return Promise.resolve({
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email@mail.com',
-          password: 'hashed_password',
-          accessToken: 'any_token'
-        })
-      }
-    }
-    const getAccountUseCaseStub: IGetAccountUseCase =
-      new GetAccountUseCaseStub()
-    const sut = new GetAccountController(getAccountUseCaseStub)
+    const { sut, getAccountUseCaseStub } = makeSut()
     const getSpy = jest.spyOn(getAccountUseCaseStub, 'get')
-    const httpRequest: IHttpRequest = {
-      body: {
-        id: 'any_id'
-      }
-    }
-    await sut.perform(httpRequest)
+
+    await sut.perform(makeFakeValidRequest())
+
     expect(getSpy).toHaveBeenCalledWith('any_id')
   })
 })
