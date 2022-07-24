cb8bae09813cb80a70929178617ffead34e95dc5
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-24 12:15:51 -0300

    test: ensure GetAccountByAccessTokenRepository returns an client account using the provided access token

---
+++ src/infra/repository/database/mongodb/account/get-account-by-access-token/get-account-by-access-token-repository.ts
@@ -0,0 +1,18 @@
+import mongoHelper from '../../helpers/mongo-helper'
+import {
+  IAccountEntitie,
+  IGetAccountByAccessTokenRepository
+} from './get-account-by-access-token-repository-protocols'
+
+export class GetAccountByAccessTokenRepository
+  implements IGetAccountByAccessTokenRepository
+{
+  async get(
+    accessToken: string,
+    isAdmin?: boolean
+  ): Promise<IAccountEntitie | null> {
+    const collectionRef = mongoHelper.getCollection('accounts')
+    const document = await collectionRef.findOne({ accessToken, isAdmin })
+    return document ? mongoHelper.replaceMongoID(document) : null
+  }
+}
