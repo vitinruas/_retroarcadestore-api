faa12583f417e92d30176d6cdde7ebba10a6067b
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-24 17:53:42 -0300

    test: ensure GetClientByUIDRepository returns an account with provided uid

---
+++ src/infra/repository/database/mongodb/client/get-account-by-uid/test/get-account-by-uid-repository.spec.ts
@@ -0,0 +1,50 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import { Collection } from 'mongoose'
+import mongoHelper from '../../../helpers/mongo-helper'
+import { GetClientByUIDRepository } from '../get-account-by-uid-repository'
+
+let mongod: MongoMemoryServer
+let collectionRef: Collection
+
+beforeAll(async () => {
+  mongod = await MongoMemoryServer.create()
+  const uri = mongod.getUri()
+  await mongoHelper.connect(uri)
+})
+
+afterAll(async () => {
+  await mongoHelper.disconnect()
+  await mongod.stop()
+})
+
+beforeEach(async () => {
+  collectionRef = mongoHelper.getCollection('accounts')
+  await collectionRef.deleteMany({})
+})
+
+const makeSut = (): GetClientByUIDRepository => {
+  return new GetClientByUIDRepository()
+}
+
+describe('GetClientByUIDRepository', () => {
+  test('should return an account with provided uid', async () => {
+    const fakeValidAccount = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'hashed_password',
+      accessToken: 'any_token'
+    }
+    const createdAccountID = (
+      await collectionRef.insertOne(fakeValidAccount)
+    ).insertedId.toString()
+    const sut = makeSut()
+
+    const account = await sut.get(createdAccountID)
+
+    expect(account!.uid).toBeTruthy()
+    expect(account!.name).toBe('any_name')
+    expect(account!.email).toBe('any_email@mail.com')
+    expect(account!.password).toBe('hashed_password')
+    expect(account!.accessToken).toBe('any_token')
+  })
+})
