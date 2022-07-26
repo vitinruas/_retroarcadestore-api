1ff19b3186301efd20af84dfeb1bce4ebef8ff68
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-26 10:17:10 -0300

    test: ensure UpdateClientController calls EmailValidator with an email

--- src/presentation/controllers/client/update/test/update-client-controller.spec.ts
+++ src/presentation/controllers/client/update/test/update-client-controller.spec.ts
@@ -1,18 +1,71 @@
 import { NoFieldProvidedError } from '../../../../errors/no-field-provided'
 import { badRequest } from '../../../../helpers/http-response-helper'
+import { IEmailValidatorAdapter } from '../../../../protocols/email-validator-protocol'
 import {
   IHttpRequest,
   IHttpResponse
 } from '../../get/get-client-controller-protocols'
 import { UpdateClientController } from '../update-client-controller'
 
+const makeFakeValidRequest = (): IHttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    photo: 'any_photo',
+    street: 'any_street',
+    postalCode: 'any_postalcode',
+    complement: 'any_complement',
+    district: 'any_district',
+    city: 'any_city',
+    country: 'any_contry'
+  }
+})
+
+const makeEmailValidatorAdapterStub = (): IEmailValidatorAdapter => {
+  class EmailValidatorAdapterStub implements IEmailValidatorAdapter {
+    validate(email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorAdapterStub()
+}
+
+interface ISut {
+  sut: UpdateClientController
+  emailValidatorAdapterStub: IEmailValidatorAdapter
+}
+
+const makeSut = (): ISut => {
+  const emailValidatorAdapterStub: IEmailValidatorAdapter =
+    makeEmailValidatorAdapterStub()
+  const sut: UpdateClientController = new UpdateClientController(
+    emailValidatorAdapterStub
+  )
+  return {
+    sut,
+    emailValidatorAdapterStub
+  }
+}
+
 describe('UpdateClientController', () => {
   test('should return 400 if no field is provided', async () => {
-    const sut = new UpdateClientController()
+    const { sut } = makeSut()
     const httpRequest: IHttpRequest = {
       body: {}
     }
+
     const httpResponse: IHttpResponse = await sut.perform(httpRequest)
+
     expect(httpResponse).toEqual(badRequest(new NoFieldProvidedError()))
+  })
+
+  test('should call EmailValidator with an email', async () => {
+    const { sut, emailValidatorAdapterStub } = makeSut()
+    const validateSpy = jest.spyOn(emailValidatorAdapterStub, 'validate')
+
+    await sut.perform(makeFakeValidRequest())
+
+    expect(validateSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
