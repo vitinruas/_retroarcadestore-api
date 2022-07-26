affaf3fe4968a17f26167ef9a1b813f811c7582e
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-26 10:20:56 -0300

    feat: ensure UpdateClientController returns 500 if EmailValidatorAdapter throws

--- src/presentation/controllers/client/update/update-client-controller.ts
+++ src/presentation/controllers/client/update/update-client-controller.ts
@@ -1,5 +1,9 @@
 import { NoFieldProvidedError } from '../../../errors/no-field-provided'
-import { badRequest, ok } from '../../../helpers/http-response-helper'
+import {
+  badRequest,
+  ok,
+  serverError
+} from '../../../helpers/http-response-helper'
 import { IEmailValidatorAdapter } from '../../../protocols/email-validator-protocol'
 import {
   IController,
@@ -10,36 +14,40 @@ import {
 export class UpdateClientController implements IController {
   constructor(private readonly emailValidatorAdapter: IEmailValidatorAdapter) {}
   async perform(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const upgradableClientFields: ReadonlyArray<string> = [
-      'name',
-      'email',
-      'password',
-      'photo'
-    ]
-    const upgradableClientAddressFields: ReadonlyArray<string> = [
-      'street',
-      'postalCode',
-      'complement',
-      'district',
-      'city',
-      'country'
-    ]
-    const clientFieldsToUpdate: ReadonlyArray<string> =
-      upgradableClientFields.filter((field: string) => {
-        return httpRequest.body[field]
-      })
+    try {
+      const upgradableClientFields: ReadonlyArray<string> = [
+        'name',
+        'email',
+        'password',
+        'photo'
+      ]
+      const upgradableClientAddressFields: ReadonlyArray<string> = [
+        'street',
+        'postalCode',
+        'complement',
+        'district',
+        'city',
+        'country'
+      ]
+      const clientFieldsToUpdate: ReadonlyArray<string> =
+        upgradableClientFields.filter((field: string) => {
+          return httpRequest.body[field]
+        })
 
-    const addressFieldsToUpdate: ReadonlyArray<string> =
-      upgradableClientAddressFields.filter((field: string) => {
-        return httpRequest.body[field]
-      })
+      const addressFieldsToUpdate: ReadonlyArray<string> =
+        upgradableClientAddressFields.filter((field: string) => {
+          return httpRequest.body[field]
+        })
 
-    if (!clientFieldsToUpdate.length && !addressFieldsToUpdate.length) {
-      return badRequest(new NoFieldProvidedError())
+      if (!clientFieldsToUpdate.length && !addressFieldsToUpdate.length) {
+        return badRequest(new NoFieldProvidedError())
+      }
+
+      this.emailValidatorAdapter.validate(httpRequest.body.email)
+
+      return Promise.resolve(ok())
+    } catch (error: any) {
+      return serverError(error)
     }
-
-    this.emailValidatorAdapter.validate(httpRequest.body.email)
-
-    return Promise.resolve(ok())
   }
 }
