1aaee5e03e1b1c6ea06b6a6396e48dbe03362180
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-27 09:40:40 -0300

    feat: ensure UpdateClientUseCase calls GetClientByUIDRepository with an uid

--- src/usecases/client/update-client/update-client-usecase.ts
+++ src/usecases/client/update-client/update-client-usecase.ts
@@ -5,9 +5,11 @@ import {
 import { IHashComparer } from '../../protocols/cryptography/hash-comparer-protocol'
 import { IHasher } from '../../protocols/cryptography/hasher-protocol'
 import { IUpdateClientRepository } from '../../protocols/repository/client/update-client-repository-protocol'
+import { IGetClientByUIDRepository } from '../get-client/get-client-usecase-protocols'
 
 export class UpdateClientUseCase implements IUpdateClientUseCase {
   constructor(
+    private readonly getClientByUIDRepository: IGetClientByUIDRepository,
     private readonly passwordHashComparerAdapter: IHashComparer,
     private readonly passwordHasherAdapter: IHasher,
     private readonly updateClientRepository: IUpdateClientRepository
@@ -16,6 +18,7 @@ export class UpdateClientUseCase implements IUpdateClientUseCase {
   async update(fields: IUpdateClientUseCaseModel): Promise<boolean> {
     // check if a password was provided
     if (fields.password) {
+      const account = await this.getClientByUIDRepository.get(fields.uid)
       // check if the password is valid
       const isValid = await this.passwordHashComparerAdapter.compare(
         fields.password,
