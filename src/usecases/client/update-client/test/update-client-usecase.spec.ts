281bde1773018b299f6a7ebd988972b5497da8e2
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-27 09:06:05 -0300

    test: ensure UpdateClientUseCase calls PasswordHashComparerAdapter with a password

--- src/usecases/client/update-client/test/update-client-usecase.spec.ts
+++ src/usecases/client/update-client/test/update-client-usecase.spec.ts
@@ -1,7 +1,17 @@
 import { IUpdateClientUseCaseModel } from '../../../../domain/usecases/client/update-client-usecase'
+import { IHashComparer } from '../../../protocols/cryptography/hash-comparer-protocol'
 import { IHasher } from '../../../protocols/cryptography/hasher-protocol'
 import { IUpdateClientRepository } from '../../../protocols/repository/client/update-client-repository-protocol'
+import { IGetClientModel } from '../../get-client/get-client-usecase-protocols'
 import { UpdateClientUseCase } from '../update-client-usecase'
+
+const makeFakeValidAccount = (): IGetClientModel => ({
+  uid: 'any_uid',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  createdAt: 'any_date',
+  authenticatedAt: 'any_date'
+})
 
 const makePasswordHasherAdapterStub = () => {
   class PasswordHasherStub implements IHasher {
@@ -14,40 +24,65 @@ const makePasswordHasherAdapterStub = () => {
 
 const makeUpdateClientRepositoryStub = () => {
   class UpdateClientRepositoryStub implements IUpdateClientRepository {
-    async update(data: IUpdateClientUseCaseModel): Promise<void> {
-      return Promise.resolve()
+    async update(data: IUpdateClientUseCaseModel): Promise<IGetClientModel> {
+      return Promise.resolve(makeFakeValidAccount())
     }
   }
   return new UpdateClientRepositoryStub()
 }
 
+const makePasswordHashComparerAdapterStub = () => {
+  class PasswordHashComparerAdapterStub implements IHashComparer {
+    compare(value: string, hash: string): Promise<boolean> {
+      return Promise.resolve(true)
+    }
+  }
+  return new PasswordHashComparerAdapterStub()
+}
+
 interface ISut {
   sut: UpdateClientUseCase
+  passwordHashComparerAdapterStub: IHashComparer
   passwordHasherAdapterStub: IHasher
   updateClientRepositoryStub: IUpdateClientRepository
 }
 
 const makeSut = (): ISut => {
+  const passwordHashComparerAdapterStub = makePasswordHashComparerAdapterStub()
   const passwordHasherAdapterStub = makePasswordHasherAdapterStub()
   const updateClientRepositoryStub = makeUpdateClientRepositoryStub()
   const sut = new UpdateClientUseCase(
+    passwordHashComparerAdapterStub,
     passwordHasherAdapterStub,
     updateClientRepositoryStub
   )
   return {
     sut,
     passwordHasherAdapterStub,
-    updateClientRepositoryStub
+    updateClientRepositoryStub,
+    passwordHashComparerAdapterStub
   }
 }
 
 describe('UpdateClientUseCase', () => {
-  test('should call PasswordHasher with a password', async () => {
+  test('should call PasswordHashComparerAdapter with a password', async () => {
+    const { sut, passwordHashComparerAdapterStub } = makeSut()
+    const compareSpy = jest.spyOn(passwordHashComparerAdapterStub, 'compare')
+
+    await sut.update({
+      password: 'any_password'
+    })
+
+    expect(compareSpy).toHaveBeenCalledWith('any_password', '')
+  })
+
+  test('should call PasswordHasher with a newPassword', async () => {
     const { sut, passwordHasherAdapterStub } = makeSut()
     const hashSpy = jest.spyOn(passwordHasherAdapterStub, 'hash')
 
     await sut.update({
-      password: 'new_password'
+      password: 'any_password',
+      newPassword: 'new_password'
     })
 
     expect(hashSpy).toHaveBeenCalledWith('new_password')
@@ -59,11 +94,12 @@ describe('UpdateClientUseCase', () => {
       .spyOn(passwordHasherAdapterStub, 'hash')
       .mockImplementationOnce(async () => Promise.reject(new Error()))
 
-    const promise: Promise<void> = sut.update({
-      password: 'new_password'
+    const promise: Promise<boolean> = sut.update({
+      password: 'any_password',
+      newPassword: 'new_password'
     })
 
-    expect(promise).rejects.toThrow()
+    await expect(promise).rejects.toThrow()
   })
 
   test('should call UpdateClientRepository correct values', async () => {
@@ -72,12 +108,14 @@ describe('UpdateClientUseCase', () => {
 
     await sut.update({
       name: 'new_name',
-      password: 'new_password'
+      password: 'any_password',
+      newPassword: 'hashed_password'
     })
 
     expect(updateSpy).toHaveBeenCalledWith({
       name: 'new_name',
-      password: 'hashed_password'
+      password: 'any_password',
+      newPassword: 'hashed_password'
     })
   })
 })
