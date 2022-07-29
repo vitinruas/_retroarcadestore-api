c3f0b36c5bfbaea5d00fe2ec831ecd09cb16c9ce
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-29 11:27:13 -0300

    feat: ensure UpdateClientAddressRepository update client address

---
+++ src/infra/repository/database/mongodb/client/update-client-address/update-client-address-repository.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+import mongoHelper from '../../helpers/mongo-helper'
+import {
+  IUpdateClientAddressRepository,
+  IUpdateClientAddressRepositoryModel
+} from './update-client-client-repository-protocols'
+
+export class UpdateClientAddressRepository
+  implements IUpdateClientAddressRepository
+{
+  async update(
+    dataToUpdate: IUpdateClientAddressRepositoryModel
+  ): Promise<void> {
+    const { uid, ...dataWithoutID } = dataToUpdate
+    const collectionRef = mongoHelper.getCollection('addresses')
+    await collectionRef.updateOne(
+      { _id: new mongoose.Types.ObjectId(uid) },
+      {
+        $set: {
+          ...dataWithoutID
+        }
+      }
+    )
+  }
+}
---
+++ src/infra/repository/database/mongodb/client/update-client-address/update-client-client-repository-protocols.ts
@@ -0,0 +1,2 @@
+export * from '../../../../../../usecases/protocols/repository/client/update-client-address-repository-protocols'
+export * from '../../../../../../domain/usecases/client/update-client-usecase'
