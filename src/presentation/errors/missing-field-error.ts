578868ab25a50e20fcc556371d97ef36f0c5b5d3
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-28 23:02:57 -0300

    refactor: change MissingFieldError message

--- src/presentation/errors/missing-field-error.ts
+++ src/presentation/errors/missing-field-error.ts
@@ -1,6 +1,6 @@
 export class MissingFieldError extends Error {
   constructor(field: string) {
-    super(`Missing field: ${field}`)
+    super(`Missing field value: ${field}`)
     this.name = 'MissingFieldError'
   }
 }
