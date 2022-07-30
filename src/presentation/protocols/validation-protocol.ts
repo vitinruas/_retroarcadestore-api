f6cc467cc2cefce68c6cfa7a54c96e4faa59cb00
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-30 17:29:40 -0300

    feat: ensure ValidationComposite returns the first instance error if validation fails

--- src/presentation/protocols/validation-protocol.ts
+++ src/presentation/protocols/validation-protocol.ts
@@ -1,3 +1,3 @@
 export interface IValidation {
-  validate(fields: any): Promise<any>
+  validate(fields: any): Promise<Error | void>
 }
--- src/validation/validation-composite.ts
+++ src/validation/validation-composite.ts
@@ -2,9 +2,10 @@ import { IValidation } from '../presentation/protocols/validation-protocol'
 
 export class ValidationComposite implements IValidation {
   constructor(private readonly validations: IValidation[]) {}
-  async validate(fields: any): Promise<Error | void> {
-    this.validations.map(async (validation) => {
-      await validation.validate(fields)
-    })
+  validate(fields: any): any {
+    for (const validation of this.validations) {
+      const error = validation.validate(fields)
+      if (error) return error
+    }
   }
 }
