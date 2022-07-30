6e26915a4b50d31398f9d5be1ed55077872a3ce1
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-30 17:55:46 -0300

    refactor: remove LoginController validations

--- src/presentation/controllers/account/authentication/login/login-controller.ts
+++ src/presentation/controllers/account/authentication/login/login-controller.ts
@@ -1,42 +1,22 @@
-import { InvalidFieldError, MissingFieldError } from '../../../../errors'
 import { UnauthenticatedLoginError } from '../../../../errors/unauthenticated-error'
 import {
-  badRequest,
   ok,
   serverError,
   unauthorized
 } from '../../../../helpers/http-response-helper'
 import {
   IController,
-  IEmailValidatorAdapter,
   IHttpRequest,
   IHttpResponse,
   IAuthenticationUseCase
 } from './login-controller-protocols'
 
 export class LoginController implements IController {
-  constructor(
-    private readonly emailValidator: IEmailValidatorAdapter,
-    private readonly authentication: IAuthenticationUseCase
-  ) {}
+  constructor(private readonly authentication: IAuthenticationUseCase) {}
 
   async perform(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
-      // check if all required fields has been provided
-      const requiredFields = ['email', 'password']
-      for (const requiredField of requiredFields) {
-        if (!httpRequest.body[requiredField]) {
-          return badRequest(new MissingFieldError(requiredField))
-        }
-      }
-
       const { email, password } = httpRequest.body
-
-      // check if the provided email is valid
-      const isValid = this.emailValidator.validate(email)
-      if (!isValid) {
-        return badRequest(new InvalidFieldError('email'))
-      }
 
       const accessToken = await this.authentication.authenticate({
         email,
--- src/presentation/controllers/account/authentication/login/test/login-controller.spec.ts
+++ src/presentation/controllers/account/authentication/login/test/login-controller.spec.ts
@@ -1,14 +1,11 @@
 import { LoginController } from '../login-controller'
-import { InvalidFieldError, MissingFieldError } from '../../../../../errors'
 import { UnauthenticatedLoginError } from '../../../../../errors/unauthenticated-error'
 import {
-  badRequest,
   ok,
   serverError,
   unauthorized
 } from '../../../../../helpers/http-response-helper'
 import {
-  IEmailValidatorAdapter,
   IHttpRequest,
   IHttpResponse,
   IAuthenticationUseCase,
@@ -21,15 +18,6 @@ const makeFakeValidRequest = (): IHttpRequest => ({
     password: 'any_password'
   }
 })
-
-const makeEmailValidatorAdapterStub = (): IEmailValidatorAdapter => {
-  class EmailValidatorAdapterStub implements IEmailValidatorAdapter {
-    validate(email: string): boolean {
-      return true
-    }
-  }
-  return new EmailValidatorAdapterStub()
-}
 
 const makeAuthenticationUseCaseStub = (): IAuthenticationUseCase => {
   class AuthenticationUseCaseStub implements IAuthenticationUseCase {
@@ -44,88 +32,20 @@ const makeAuthenticationUseCaseStub = (): IAuthenticationUseCase => {
 
 interface ISut {
   sut: LoginController
-  emailValidatorAdapterStub: IEmailValidatorAdapter
   authenticationUseCaseStub: IAuthenticationUseCase
 }
 
 const makeSut = (): ISut => {
-  const emailValidatorAdapterStub: IEmailValidatorAdapter =
-    makeEmailValidatorAdapterStub()
   const authenticationUseCaseStub: IAuthenticationUseCase =
     makeAuthenticationUseCaseStub()
-  const sut: LoginController = new LoginController(
-    emailValidatorAdapterStub,
-    authenticationUseCaseStub
-  )
+  const sut: LoginController = new LoginController(authenticationUseCaseStub)
   return {
     sut,
-    emailValidatorAdapterStub,
     authenticationUseCaseStub
   }
 }
 
 describe('LoginController', () => {
-  test('should return 400 if no email is provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest: IHttpRequest = {
-      body: {
-        password: 'any_password'
-      }
-    }
-
-    const httpResponse: IHttpResponse = await sut.perform(httpRequest)
-
-    expect(httpResponse).toEqual(badRequest(new MissingFieldError('email')))
-  })
-
-  test('should return 400 if no password is provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest: IHttpRequest = {
-      body: {
-        email: 'any_email@mail.com'
-      }
-    }
-
-    const httpResponse: IHttpResponse = await sut.perform(httpRequest)
-
-    expect(httpResponse).toEqual(badRequest(new MissingFieldError('password')))
-  })
-
-  test('should call EmailValidatorAdapter with an email', async () => {
-    const { sut, emailValidatorAdapterStub } = makeSut()
-    const validateSpy = jest.spyOn(emailValidatorAdapterStub, 'validate')
-
-    await sut.perform(makeFakeValidRequest())
-
-    expect(validateSpy).toHaveBeenCalledWith('any_email@mail.com')
-  })
-
-  test('should return 500 if EmailValidatorAdapter throws', async () => {
-    const { sut, emailValidatorAdapterStub } = makeSut()
-    jest
-      .spyOn(emailValidatorAdapterStub, 'validate')
-      .mockImplementationOnce(() => {
-        throw new Error()
-      })
-
-    const httpResponse: IHttpResponse = await sut.perform(
-      makeFakeValidRequest()
-    )
-
-    expect(httpResponse).toEqual(serverError(new Error()))
-  })
-
-  test('should return 400 if an invalid email is provided', async () => {
-    const { sut, emailValidatorAdapterStub } = makeSut()
-    jest.spyOn(emailValidatorAdapterStub, 'validate').mockReturnValueOnce(false)
-
-    const httpResponse: IHttpResponse = await sut.perform(
-      makeFakeValidRequest()
-    )
-
-    expect(httpResponse).toEqual(badRequest(new InvalidFieldError('email')))
-  })
-
   test('should call AuthenticationUseCase with correct values', async () => {
     const { sut, authenticationUseCaseStub } = makeSut()
     const authenticateSpy = jest.spyOn(
