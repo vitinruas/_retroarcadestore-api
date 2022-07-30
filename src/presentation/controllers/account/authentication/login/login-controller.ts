b4bee681f6ff1ae56e87c57f6d2ada78b54fc04a
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-30 18:08:02 -0300

    feat: ensure LoginController calls ValidationComposite with correct values

--- src/presentation/controllers/account/authentication/login/login-controller.ts
+++ src/presentation/controllers/account/authentication/login/login-controller.ts
@@ -6,18 +6,22 @@ import {
 } from '../../../../helpers/http-response-helper'
 import {
   IController,
+  IValidation,
   IHttpRequest,
   IHttpResponse,
   IAuthenticationUseCase
 } from './login-controller-protocols'
 
 export class LoginController implements IController {
-  constructor(private readonly authentication: IAuthenticationUseCase) {}
+  constructor(
+    private readonly validationComposite: IValidation,
+    private readonly authentication: IAuthenticationUseCase
+  ) {}
 
   async perform(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
+      await this.validationComposite.validate(httpRequest.body)
       const { email, password } = httpRequest.body
-
       const accessToken = await this.authentication.authenticate({
         email,
         password
