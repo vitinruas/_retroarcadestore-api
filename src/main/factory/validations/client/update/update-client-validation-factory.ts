57bacbc7f3b7ed49ff90c60906fbeb7e40cdec39
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-31 14:42:37 -0300

    feat: create UpdateClientValidationFactory

---
+++ src/main/factory/validations/client/update/update-client-validation-factory.ts
@@ -0,0 +1,26 @@
+import { IValidation } from './update-client-validation-factory-protocols'
+import { EmailValidatorAdapter } from '../../../../../utils/email-validator/email-validator-adapter'
+import { ValidationComposite } from '../../../../../validation/validation-composite'
+import {
+  CompareFieldsValidation,
+  EmailValidation,
+  LengthFieldValidation
+} from '../../../../../validation/validations'
+import { RequiredFieldIfThereisAnother } from '../../../../../validation/validations/required-field-if-there-is-another/required-field-if-there-is-another-validation'
+
+export const makeUpdateClientValidationFactory = () => {
+  const validations: IValidation[] = []
+  validations.push(new LengthFieldValidation('name', 3, 32))
+  validations.push(new RequiredFieldIfThereisAnother('email', 'password'))
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+  validations.push(
+    new RequiredFieldIfThereisAnother('newPasswordConfirmation', 'newPassword')
+  )
+  validations.push(new LengthFieldValidation('newPassword', 8))
+  validations.push(new RequiredFieldIfThereisAnother('newPassword', 'password'))
+  validations.push(
+    new CompareFieldsValidation('newPassword', 'newPasswordConfirmation')
+  )
+  validations.push(new LengthFieldValidation('postalCode', 5, 10))
+  return new ValidationComposite(validations)
+}
