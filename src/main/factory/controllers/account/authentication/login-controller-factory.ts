c689cb2a186c7e8f0e0713bae7df78b954158013
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-31 12:08:07 -0300

    feat: create LoginValidationFactory

--- src/main/factory/controllers/account/authentication/login-controller-factory.ts
+++ src/main/factory/controllers/account/authentication/login-controller-factory.ts
@@ -1,11 +1,11 @@
 import { LoginController } from '../../../../../presentation/controllers/account/authentication/login/login-controller'
 import { IController } from '../../../../../presentation/protocols/controller-protocol'
-import { EmailValidatorAdapter } from '../../../../../utils/email-validator/email-validator-adapter'
 import { makeAuthenticationUseCaseFactory } from '../../../usecases/account/authentication/authentication-usecase-factory'
+import { makeLoginValidationFactory } from '../../../validations/login/login-validation-factory'
 
 export const makeLoginControllerFactory = (): IController => {
   const loginController = new LoginController(
-    new EmailValidatorAdapter(),
+    makeLoginValidationFactory(),
     makeAuthenticationUseCaseFactory()
   )
   return loginController
