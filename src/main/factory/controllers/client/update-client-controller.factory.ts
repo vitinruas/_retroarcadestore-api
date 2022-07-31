e46870f63f3fbfdab34ce00cf30381cbc9090b76
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-31 14:43:18 -0300

    test: create UpdateClientValidationFactory

--- src/main/factory/controllers/client/update-client-controller.factory.ts
+++ src/main/factory/controllers/client/update-client-controller.factory.ts
@@ -1,11 +1,10 @@
 import { UpdateClientController } from '../../../../presentation/controllers/client/update/update-client-controller'
-import { EmailValidatorAdapter } from '../../../../utils/email-validator/email-validator-adapter'
 import { makeUpdateClientUseCase } from '../../usecases/client/update-client/update-client-usecase-factory'
+import { makeUpdateClientValidationFactory } from '../../validations/client/update/update-client-validation-factory'
 
 export const makeUpdateClientController = (): UpdateClientController => {
-  const emailValidator = new EmailValidatorAdapter()
   const updateClientController = new UpdateClientController(
-    emailValidator,
+    makeUpdateClientValidationFactory(),
     makeUpdateClientUseCase()
   )
   return updateClientController
