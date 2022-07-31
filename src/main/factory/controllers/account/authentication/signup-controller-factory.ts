99ab77365a7bede6fb608a01a62150fa0244dab8
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-31 12:36:32 -0300

    feat: create SignUpValidationFactory

--- src/main/factory/controllers/account/authentication/signup-controller-factory.ts
+++ src/main/factory/controllers/account/authentication/signup-controller-factory.ts
@@ -1,11 +1,11 @@
 import { SignUpController } from '../../../../../presentation/controllers/account/authentication/signup/signup-controller'
 import { IController } from '../../../../../presentation/protocols/controller-protocol'
-import { EmailValidatorAdapter } from '../../../../../utils/email-validator/email-validator-adapter'
 import { makeAddAccountUseCaseFactory } from '../../../usecases/account/add-account/add-account-usecase-factory'
+import { makeSignUpValidationFactory } from '../../../validations/signup/signup-validation-factory'
 
 export const makeSignUpControllerFactory = (): IController => {
   const signUpController = new SignUpController(
-    new EmailValidatorAdapter(),
+    makeSignUpValidationFactory(),
     makeAddAccountUseCaseFactory()
   )
   return signUpController
