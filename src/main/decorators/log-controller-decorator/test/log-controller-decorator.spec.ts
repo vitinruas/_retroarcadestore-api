ebab8c1524abc97436bfc8cd56a502fd082cc441
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-31 16:32:08 -0300

    test: ensure LogController calls Controller with correct values

---
+++ src/main/decorators/log-controller-decorator/test/log-controller-decorator.spec.ts
@@ -0,0 +1,50 @@
+import { noContent } from '../../../../presentation/helpers/http-response-helper'
+import {
+  IController,
+  IHttpRequest,
+  IHttpResponse
+} from '../../../../presentation/protocols'
+import { LogControllerDecorator } from '../log-controller-decorators'
+
+const makeFakeValidRequest = (): IHttpRequest => ({
+  headers: undefined,
+  file: undefined,
+  files: undefined,
+  body: {
+    value: 'any_value'
+  }
+})
+
+const makeControllerStub = (): IController => {
+  class ControllerStub implements IController {
+    perform(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+      return Promise.resolve(noContent())
+    }
+  }
+  return new ControllerStub()
+}
+
+interface ISut {
+  sut: LogControllerDecorator
+  controllerStub: IController
+}
+
+const makeSut = (): ISut => {
+  const controllerStub: IController = makeControllerStub()
+  const sut: LogControllerDecorator = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('LogControllerDecorator', () => {
+  test('should call controller with correct values', async () => {
+    const { sut, controllerStub } = makeSut()
+    const performSpy = jest.spyOn(controllerStub, 'perform')
+
+    await sut.perform(makeFakeValidRequest())
+
+    expect(performSpy).toHaveBeenCalledWith(makeFakeValidRequest())
+  })
+})
