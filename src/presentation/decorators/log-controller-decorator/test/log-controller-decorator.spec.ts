2efd96f011f2491cf8ea2918fabcb8cc34305c70
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-31 17:00:36 -0300

    test: ensure LogControllerDecorator calls LogControllerUseCase with correct values

--- src/presentation/decorators/log-controller-decorator/test/log-controller-decorator.spec.ts
+++ src/presentation/decorators/log-controller-decorator/test/log-controller-decorator.spec.ts
@@ -7,12 +7,20 @@ import {
   IHttpRequest,
   IHttpResponse
 } from '../../../../presentation/protocols'
+import { ILogControllerUseCase } from '../../../../domain/usecases/system/log/log-controller-usecase'
 import { LogControllerDecorator } from '../log-controller-decorators'
 
 const makeFakeValidRequest = (): IHttpRequest => ({
   headers: undefined,
   file: undefined,
   files: undefined,
+  body: {
+    value: 'any_value'
+  }
+})
+
+const makeFakeValidResponse = (): IHttpResponse => ({
+  statusCode: 200,
   body: {
     value: 'any_value'
   }
@@ -27,28 +35,59 @@ const makeControllerStub = (): IController => {
   return new ControllerStub()
 }
 
+const makeLogControllerUseCaseStub = (): ILogControllerUseCase => {
+  class LogControllerUseCaseStub implements ILogControllerUseCase {
+    log(request: IHttpRequest, response: IHttpResponse): Promise<void> {
+      return Promise.resolve()
+    }
+  }
+  return new LogControllerUseCaseStub()
+}
+
 interface ISut {
   sut: LogControllerDecorator
+  logControllerUseCaseStub: ILogControllerUseCase
   controllerStub: IController
 }
 
 const makeSut = (): ISut => {
+  const logControllerUseCaseStub: ILogControllerUseCase =
+    makeLogControllerUseCaseStub()
   const controllerStub: IController = makeControllerStub()
-  const sut: LogControllerDecorator = new LogControllerDecorator(controllerStub)
+  const sut: LogControllerDecorator = new LogControllerDecorator(
+    logControllerUseCaseStub,
+    controllerStub
+  )
   return {
     sut,
+    logControllerUseCaseStub,
     controllerStub
   }
 }
 
 describe('LogControllerDecorator', () => {
-  test('should call controller with correct values', async () => {
+  test('should call Controller with correct values', async () => {
     const { sut, controllerStub } = makeSut()
     const performSpy = jest.spyOn(controllerStub, 'perform')
 
     await sut.perform(makeFakeValidRequest())
 
     expect(performSpy).toHaveBeenCalledWith(makeFakeValidRequest())
+  })
+
+  test('should call LogControllerUseCase with correct values', async () => {
+    const { sut, controllerStub, logControllerUseCaseStub } = makeSut()
+    jest
+      .spyOn(controllerStub, 'perform')
+      .mockReturnValue(Promise.resolve(makeFakeValidResponse()))
+    const logSpy = jest.spyOn(logControllerUseCaseStub, 'log')
+
+    await sut.perform(makeFakeValidRequest())
+
+    expect(logSpy).toHaveBeenCalledWith(
+      makeFakeValidRequest(),
+      makeFakeValidResponse()
+    )
   })
 
   test('should returns the same controller response', async () => {
