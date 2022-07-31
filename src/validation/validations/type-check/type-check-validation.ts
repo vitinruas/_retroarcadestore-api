33d855a3f7c56cfc9aa35f36b170179dc78b1634
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-31 15:05:35 -0300

    feat: ensure TypeCheckValidation returns an error if field type is invalid

---
+++ src/validation/validations/type-check/type-check-validation.ts
@@ -0,0 +1,22 @@
+import { InvalidFieldError } from '../validations-errors'
+import { IValidation } from '../validations-protocols'
+
+type Types = 'string' | 'number' | 'boolean'
+
+export class TypeCheckValidation implements IValidation {
+  constructor(
+    private readonly fieldName: string,
+    private readonly type: Types
+  ) {}
+
+  validate(fields: any): any {
+    switch (this.type) {
+      case 'number':
+        if (!Number(fields[this.fieldName]))
+          return new InvalidFieldError(this.fieldName)
+        break
+      default:
+        break
+    }
+  }
+}
