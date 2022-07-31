a674beba59dc52ff230f47b6cff8c8541ed82f85
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-07-31 11:25:19 -0300

    test: ensure LoginValidationFactory calls ValidationComposite with Login validations

---
+++ src/main/factory/validations/login/test/login-validation-factory.spec.ts
@@ -0,0 +1,21 @@
+import { IValidation } from '../../../../../presentation/protocols'
+import { EmailValidatorAdapter } from '../../../../../utils/email-validator/email-validator-adapter'
+import { ValidationComposite } from '../../../../../validation/validation-composite'
+import {
+  EmailValidation,
+  RequiredFieldValidation
+} from '../../../../../validation/validations'
+import { makeLoginValidationFactory } from '../login-validation-factory'
+
+jest.mock('../../../../../validation/validation-composite')
+describe('LoginValidationFactory', () => {
+  test('should call ValidationComposite with Login validations', () => {
+    makeLoginValidationFactory()
+    const validations: IValidation[] = []
+    for (const field of ['email', 'password']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
