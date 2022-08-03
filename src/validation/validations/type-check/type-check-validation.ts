c999f639637371db4d555866f49cff92cbc4d5fc
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-08-03 13:43:32 -0300

    fix: ensure TypeCheckValidation validat if field value is provided

--- src/validation/validations/type-check/type-check-validation.ts
+++ src/validation/validations/type-check/type-check-validation.ts
@@ -10,13 +10,15 @@ export class TypeCheckValidation implements IValidation {
   ) {}
 
   validate(fields: any): any {
-    switch (this.type) {
-      case 'number':
-        if (!Number(fields[this.fieldName]))
-          return new InvalidFieldError(this.fieldName)
-        break
-      default:
-        break
+    if (fields[this.fieldName]) {
+      switch (this.type) {
+        case 'number':
+          if (!Number(fields[this.fieldName]))
+            return new InvalidFieldError(this.fieldName)
+          break
+        default:
+          break
+      }
     }
   }
 }
