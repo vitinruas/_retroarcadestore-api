ebe4a3989bdec85950a9097085cab08b274faacb
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-08-07 19:28:49 -0300

    feat: ensure GeoAdapter returns the Geographic informations

--- src/infra/gathering/geo/geo-adapter.ts
+++ src/infra/gathering/geo/geo-adapter.ts
@@ -5,13 +5,19 @@ import {
 import geoip from 'geoip-lite'
 
 export class GeoIPAdapter implements IGeoAdapter {
-  async lookup(ip: string): Promise<IGeoEntitie> {
-    await geoip.lookup(ip)
-    return {
-      city: 'any_city',
-      country: 'any_country',
-      coords: { latitude: 10.0, longitude: 10.0 },
-      area: 1000
+  async lookup(ip: string): Promise<IGeoEntitie | null> {
+    const geo: geoip.Lookup | null = await geoip.lookup(ip)
+    if (geo) {
+      const geoFormatted: IGeoEntitie = {
+        city: geo.city,
+        state: geo.region,
+        country: geo.country,
+        coords: { latitude: geo.ll[0], longitude: geo.ll[1] },
+        areaRadius: geo.area,
+        zipCode: null
+      }
+      return geoFormatted
     }
+    return null
   }
 }
