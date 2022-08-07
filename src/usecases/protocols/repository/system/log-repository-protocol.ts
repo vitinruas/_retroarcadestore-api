784dcd8bd5be36742058e7a3d3f79fab82308958
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-08-07 16:26:27 -0300

    feat: ensure LogControllerUseCase calls LogRepository with correct values

---
+++ src/usecases/protocols/repository/system/log-repository-protocol.ts
@@ -0,0 +1,12 @@
+import { IHttpRequest, IHttpResponse } from '../../../../presentation/protocols'
+import { IGeoEntitie } from './geo-adapter-protocol'
+
+export interface ILogModel {
+  response: IHttpResponse
+  request: IHttpRequest
+  geoInformations: IGeoEntitie
+}
+
+export interface ILogRepository {
+  log(logData: ILogModel): Promise<void>
+}
--- src/usecases/system/log/log-controller-usecase.ts
+++ src/usecases/system/log/log-controller-usecase.ts
@@ -1,11 +1,34 @@
 import { ILogControllerUseCase } from '../../../domain/usecases/system/log/log-controller-usecase'
 import { IHttpRequest, IHttpResponse } from '../../../presentation/protocols'
-import { IGeoAdapter } from '../../protocols/repository/system/geo-adapter-protocol'
+import {
+  IGeoAdapter,
+  IGeoEntitie
+} from '../../protocols/repository/system/geo-adapter-protocol'
+import {
+  ILogModel,
+  ILogRepository
+} from '../../protocols/repository/system/log-repository-protocol'
 
 export class LogControllerUseCase implements ILogControllerUseCase {
-  constructor(private readonly geoAdapter: IGeoAdapter) {}
+  constructor(
+    private readonly logRepository: ILogRepository,
+    private readonly geoAdapter: IGeoAdapter
+  ) {}
 
   async log(request: IHttpRequest, response: IHttpResponse): Promise<void> {
-    await this.geoAdapter.lookup(request.ip)
+    const geoData: IGeoEntitie = await this.geoAdapter.lookup(request.ip)
+    const logData: ILogModel = Object.assign({
+      request,
+      response,
+      geoInformations: geoData
+    })
+
+    // remove all password related data
+    delete logData.request.body.password
+    delete logData.request.body.passwordConfirmation
+    delete logData.request.body.newPassword
+    delete logData.request.body.newPasswordConfirmation
+
+    await this.logRepository.log(logData)
   }
 }
