c3454e232034abeb095c3d14aad78bcbcb95b0aa
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-08-07 19:29:27 -0300

    test: ensure GeoAdapter returns the Geographic informations

--- src/infra/gathering/geo/test/geo-adapter.spec.ts
+++ src/infra/gathering/geo/test/geo-adapter.spec.ts
@@ -1,16 +1,17 @@
 import { GeoIPAdapter } from '../geo-adapter'
 import geoip from 'geoip-lite'
+import { IGeoEntitie } from '../../../../usecases/system/log/log-controller-usecase-protocols'
 
 jest.mock('geoip-lite', () => ({
   lookup() {
     return {
       range: [1000, 1000],
       country: 'any_country',
-      region: 'any_region',
+      region: 'any_state',
       eu: 'any_europeanunion',
       timezone: 'any_timezone',
       city: 'any_city',
-      ll: { latitude: 10.0, longitude: 10.0 },
+      ll: [10.0, 10.0],
       metro: 111,
       area: 1000
     }
@@ -25,5 +26,20 @@ describe('GeoIPAdapter', () => {
     await sut.lookup('111.111.111.111')
 
     expect(lookupSpy).toHaveBeenCalledWith('111.111.111.111')
+  })
+
+  test('should return the Geographic informations', async () => {
+    const sut = new GeoIPAdapter()
+
+    const geo: IGeoEntitie | null = await sut.lookup('111.111.111.111')
+
+    expect(geo).toEqual({
+      city: 'any_city',
+      state: 'any_state',
+      country: 'any_country',
+      coords: { latitude: 10.0, longitude: 10.0 },
+      areaRadius: 1000,
+      zipCode: null
+    })
   })
 })
