e327f6451806cafd1796c6f381b5a630aacd1c21
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-08-08 20:44:03 -0300

    test: ensure GetProductsUseCase calls GetProductsRepository without PID

---
+++ src/usecases/product/get/test/get-products-usecase.spec.ts
@@ -0,0 +1,56 @@
+import { IProductEntitie } from '../../../../domain/entities/product/product-entitie'
+import { IGetProductsRepository } from '../../../protocols/repository/product/get-products-repository-protocol'
+import { GetProductsUseCase } from '../get-products-usecase'
+
+const makeFakeProduct = (): IProductEntitie => ({
+  pid: 'any_id',
+  category: 'games',
+  name: 'any_name',
+  mainImage: 'any_image',
+  previewImages: ['any_image', 'any_image', 'any_image'],
+  description: 'any_description',
+  quantity: 'isGame',
+  price: 0,
+  discount: 0,
+  createdAt: 'any_date'
+})
+
+const makeGetProductsRepositoryStub = (): IGetProductsRepository => {
+  class GetProductsRepository implements IGetProductsRepository {
+    async get(
+      id?: string
+    ): Promise<IProductEntitie | IProductEntitie[] | null> {
+      return Promise.resolve([
+        makeFakeProduct(),
+        makeFakeProduct(),
+        makeFakeProduct()
+      ])
+    }
+  }
+  return new GetProductsRepository()
+}
+
+interface ISut {
+  sut: GetProductsUseCase
+  getProductsRepositoryStub: IGetProductsRepository
+}
+
+const makeSut = (): ISut => {
+  const getProductsRepositoryStub = makeGetProductsRepositoryStub()
+  const sut = new GetProductsUseCase(getProductsRepositoryStub)
+  return {
+    sut,
+    getProductsRepositoryStub
+  }
+}
+
+describe('GetProductsUseCase', () => {
+  test('should call GetProductsRepository without PID', async () => {
+    const { sut, getProductsRepositoryStub } = makeSut()
+    const getSpy = jest.spyOn(getProductsRepositoryStub, 'get')
+
+    await sut.get()
+
+    expect(getSpy).toHaveBeenCalledWith()
+  })
+})
