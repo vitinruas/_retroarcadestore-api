fef0625fc5306270561027947b818a26d2701196
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-08-10 10:55:40 -0300

    feat: ensure AddCartProductController returns 500 if AddCartProductUseCase throws

--- src/presentation/controllers/cart/add-cart-product/add-cart-product-controller.ts
+++ src/presentation/controllers/cart/add-cart-product/add-cart-product-controller.ts
@@ -1,11 +1,15 @@
 import { IAddCartProductUseCase } from '../../../../domain/usecases/cart/add-cart-product-usecase'
-import { ok } from '../../../helpers/http-response-helper'
+import { ok, serverError } from '../../../helpers/http-response-helper'
 import { IController, IHttpRequest, IHttpResponse } from '../../../protocols'
 
 export class AddCartProductController implements IController {
   constructor(private readonly addCartProductUseCase: IAddCartProductUseCase) {}
   async perform(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    await this.addCartProductUseCase.add(httpRequest.body.pid)
-    return Promise.resolve(ok())
+    try {
+      await this.addCartProductUseCase.add(httpRequest.body.pid)
+      return Promise.resolve(ok())
+    } catch (error: any) {
+      return serverError(error)
+    }
   }
 }
