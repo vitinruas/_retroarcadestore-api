649f12acff889f67325ac5014a920d62b9488870
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-08-10 11:15:41 -0300

    feat: ensure AddCartProductController returns 400 if AddCartProductUseCase fails

--- src/presentation/controllers/cart/add-cart-product/add-cart-product-controller.ts
+++ src/presentation/controllers/cart/add-cart-product/add-cart-product-controller.ts
@@ -1,13 +1,23 @@
 import { IAddCartProductUseCase } from '../../../../domain/usecases/cart/add-cart-product-usecase'
-import { ok, serverError } from '../../../helpers/http-response-helper'
+import { InvalidFieldError } from '../../../errors'
+import {
+  badRequest,
+  noContent,
+  serverError
+} from '../../../helpers/http-response-helper'
 import { IController, IHttpRequest, IHttpResponse } from '../../../protocols'
 
 export class AddCartProductController implements IController {
   constructor(private readonly addCartProductUseCase: IAddCartProductUseCase) {}
   async perform(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
-      await this.addCartProductUseCase.add(httpRequest.body.pid)
-      return Promise.resolve(ok())
+      const productHasBeenAdded: boolean = await this.addCartProductUseCase.add(
+        httpRequest.body.pid
+      )
+      if (!productHasBeenAdded) {
+        return badRequest(new InvalidFieldError('product'))
+      }
+      return noContent()
     } catch (error: any) {
       return serverError(error)
     }
